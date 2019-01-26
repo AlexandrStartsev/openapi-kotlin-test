cee87931769ae1d8c54bbb858a0c0e2f8385aaea
Alexandr Startsev <alexandr.startsev@gmail.com>
2019-01-26 11:07:47 -0800

    cleamup

--- src/generators/kotlinx/src/main/java/edu/alex/KotlinxGenerator.java
+++ src/generators/kotlinx/src/main/java/edu/alex/KotlinxGenerator.java
@@ -155,15 +155,6 @@ public class KotlinxGenerator extends AbstractKotlinCodegen {
     supportingFiles.add(new SupportingFile("settings.gradle.mustache", "", "settings.gradle"));
 
     final String infrastructureFolder = (sourceFolder + File.separator + packageName + File.separator + "infrastructure").replace(".", "/");
-
-    supportingFiles.add(new SupportingFile("infrastructure/ApiClient.kt.mustache", infrastructureFolder, "ApiClient.kt"));
-    supportingFiles.add(new SupportingFile("infrastructure/ApiAbstractions.kt.mustache", infrastructureFolder, "ApiAbstractions.kt"));
-    supportingFiles.add(new SupportingFile("infrastructure/ApiInfrastructureResponse.kt.mustache", infrastructureFolder, "ApiInfrastructureResponse.kt"));
-    supportingFiles.add(new SupportingFile("infrastructure/ApplicationDelegates.kt.mustache", infrastructureFolder, "ApplicationDelegates.kt"));
-    supportingFiles.add(new SupportingFile("infrastructure/RequestConfig.kt.mustache", infrastructureFolder, "RequestConfig.kt"));
-    supportingFiles.add(new SupportingFile("infrastructure/RequestMethod.kt.mustache", infrastructureFolder, "RequestMethod.kt"));
-    supportingFiles.add(new SupportingFile("infrastructure/ResponseExtensions.kt.mustache", infrastructureFolder, "ResponseExtensions.kt"));
     supportingFiles.add(new SupportingFile("infrastructure/Serializer.kt.mustache", infrastructureFolder, "Serializer.kt"));
-    supportingFiles.add(new SupportingFile("infrastructure/Errors.kt.mustache", infrastructureFolder, "Errors.kt"));
   }
 }
--- src/test/kotlin/edu/alex/TestApp.kt
+++ src/test/kotlin/edu/alex/TestApp.kt
@@ -1,5 +1,6 @@
 package edu.alex
 
+import edu.alex.infrastructure.Serializer
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -9,7 +10,7 @@ class TestApp {
     @Test
     fun test1() {
 
-        val info = UserContactInfo.parse("""{"phone": "123-456-7890"}""")
+        val info = Serializer.parse<UserContactInfo>("""{"phone": "123-456-7890"}""")
 
         assertTrue { info is UserContactInfo }
         assertEquals("123-456-7890", info.phone)
@@ -18,7 +19,7 @@ class TestApp {
     @Test
     fun test2() {
 
-        val alex = User.parse("""{"id": 1, "name": "Alex", "dob": "12/04", "race": "white"}""")
+        val alex = Serializer.parse<User>("""{"id": 1, "name": "Alex", "dob": "12/04", "race": "white"}""")
 
         assertEquals(Race.white, alex.race)
         assertEquals(null, alex.skippableArray)
@@ -30,7 +31,7 @@ class TestApp {
     fun test3() {
 
         val testList = listOf(Movie(title="Star Wars"), Movie(title="Lord of the rings"))
-        val alex = User.parse("""{"id": 1, "junk": 123, "name": "Alex", "dob": "12/04", "favoriteMovies": [{"title": "Star Wars"}, {"title": "Lord of the rings"}]}""")
+        val alex = Serializer.parse<User>("""{"id": 1, "junk": 123, "name": "Alex", "dob": "12/04", "favoriteMovies": [{"title": "Star Wars"}, {"title": "Lord of the rings"}]}""")
 
 
         assertTrue { alex.favoriteMovies!!.containsAll(testList) }
@@ -40,10 +41,12 @@ class TestApp {
     fun test4() {
         /*
          TODO: doesn't work for now - outpouts nulls and empty arrays
+         val json = """{"id":1,"name":"Alex","dob":"12/04","favoriteMovies":[{"title":"Star Wars"},{"title":"Lord of the rings"}]}"""
+        */
 
-        val json = """{"id":1,"name":"Alex","dob":"12/04","favoriteMovies":[{"title":"Star Wars"},{"title":"Lord of the rings"}]}"""
-        val user = UserA(id = 1, name = "Alex", dob = "12/04", favoriteMovies = listOf(Movie(title="Star Wars"), Movie(title="Lord of the rings")), requiredSkippableArray = listOf())
+        val json = """{"id":1,"name":"Alex","dob":"12/04","requiredSkippableArray":[],"race":null,"contactInfo":null,"favoriteMovies":[{"title":"Star Wars"},{"title":"Lord of the rings"}],"skippableArray":null}"""
+        val user = User(id = 1, name = "Alex", dob = "12/04", favoriteMovies = listOf(Movie(title="Star Wars"), Movie(title="Lord of the rings")), requiredSkippableArray = listOf())
 
-        assertEquals( json, user.stringify() )*/
+        assertEquals( json, Serializer.stringify(user) )
     }
 }
